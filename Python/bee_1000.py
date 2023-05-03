27e477c32fd811f4c3724707052cbe19e99caaaa
Rayssa Buarque <102766434+RayssaBuarque@users.noreply.github.com>
2023-05-03 17:28:42 -0300

    Create bee_1000.py

---
+++ Python/bee_1000.py
@@ -0,0 +1,14 @@
+'''
+Bem-vindo ao beecrowd!
+
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
+Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+	ENTRADA: 
+		Este problema não possui entrada
+
+	SAÍDA:
+		Você deve imprimir a mensagem "Hello World!"
+'''
+
+print('Hello World!')
