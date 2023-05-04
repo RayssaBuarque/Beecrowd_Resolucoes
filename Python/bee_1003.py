ced1febae0d847476e96b86d6b0aae7e469394e9
Rayssa Buarque <102766434+RayssaBuarque@users.noreply.github.com>
2023-05-04 20:53:00 -0300

    Create bee_1003.py

---
+++ Python/bee_1003.py
@@ -0,0 +1,23 @@
+'''
+SOMA SIMPLES
+
+Leia dois valores inteiros, no caso para variáveis A e B.
+A seguir, calcule a soma entre elas e atribua à variável SOMA.
+A seguir escrever o valor desta variável.
+  
+  ENTRADA:
+    O arquivo de entrada contém 2 valores inteiros.
+    
+  SAÍDA:
+    Imprima a mensagem "SOMA" com todas as letras maiúsculas, 
+    com um espaço em branco antes e depois da igualdade seguido pelo valor correspondente à soma de A e B.
+    Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado,
+    caso contrário, você receberá "Presentation Error".
+'''
+
+a = int(input())
+b = int(input())
+
+soma = a+b
+
+print('SOMA =', soma)
