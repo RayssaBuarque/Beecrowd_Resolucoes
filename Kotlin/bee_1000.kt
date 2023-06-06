c6542231a5228bd85f7034dbf015b200d094babf
Rayssa Buarque <102766434+RayssaBuarque@users.noreply.github.com>
2023-06-06 20:52:11 -0300

    Create bee_1000.kt

---
+++ Kotlin/bee_1000.kt
@@ -0,0 +1,22 @@
+/*
+HELLO WORLD!
+
+Bem-vindo ao beecrowd!
+
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
+Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+	ENTRADA: 
+		Este problema não possui entrada
+
+	SAÍDA:
+		Você deve imprimir a mensagem "Hello World!"
+*/
+
+import java.util.*
+
+fun main(args: Array<String>) {
+
+	println("Hello World!");
+	
+}
